074a40340d59605e041c10e68f67e7e0ab8c8056
Green Gatherings <green.gatherings79@gmail.com>
2025-01-24 13:54:29 +0530

    Test change

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
+/*
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+
 
 function App() {
   const [count, setCount] = useState(0)
@@ -31,5 +33,21 @@ function App() {
     </>
   )
 }
+*/
+import reactLogo from "./assets/react.svg";
+import "./App.css";
+
+function App() {
+	return (
+			<div className="App">
+			<header className="App-header">
+			<img src={reactLogo} className="logo react" alt="React logo" />
+
+			<h1>Hello from Amplify</h1>
+			</header>
+			</div>
+	       );
+}
+
 
 export default App
