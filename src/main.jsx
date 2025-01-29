1efeadc6d4a72e3eec402ef5e77897e455693639
greengatherings <green.gatherings79@gmail.com>
2025-01-29 15:14:54 +0530

    Update main.jsx
    Added Heading

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
 	<StrictMode>
-  		<p align="center" align="center" > Welcome to Green Gatherings. We coming soon </p>
+  		<h1 align="center"> Welcome to Green Gatherings. We coming soon </h1>
 		<App />
 	</StrictMode>,
 )
